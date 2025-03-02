d769a686c6b847feb4a14f3a80c0293e3328db12
Ta Dang Duong <tadduong2004@gmail.com>
2025-03-02 08:30:41 +0700

    set up server

---
+++ backend/main.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
